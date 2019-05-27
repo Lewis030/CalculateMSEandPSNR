69e456273e022c6376b6fffb2bd4c652dde57405
Boris <boriska-94@mail.ru>
2019-05-27 23:10:46 +1000

    Calculate metrics MSE, RMSE and PSNR

---
+++ CalculateMSEandPSNR/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CalculateMSEandPSNR
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.btnOpenFirstImage = new System.Windows.Forms.Button();
+			this.btnOpenSecondImage = new System.Windows.Forms.Button();
+			this.btnCompareImage = new System.Windows.Forms.Button();
+			this.rtbMetrics = new System.Windows.Forms.RichTextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(358, 210);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Location = new System.Drawing.Point(417, 12);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(371, 210);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			// 
+			// btnOpenFirstImage
+			// 
+			this.btnOpenFirstImage.Location = new System.Drawing.Point(12, 228);
+			this.btnOpenFirstImage.Name = "btnOpenFirstImage";
+			this.btnOpenFirstImage.Size = new System.Drawing.Size(93, 23);
+			this.btnOpenFirstImage.TabIndex = 2;
+			this.btnOpenFirstImage.Text = "Open 1st Image";
+			this.btnOpenFirstImage.UseVisualStyleBackColor = true;
+			this.btnOpenFirstImage.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// btnOpenSecondImage
+			// 
+			this.btnOpenSecondImage.Location = new System.Drawing.Point(417, 228);
+			this.btnOpenSecondImage.Name = "btnOpenSecondImage";
+			this.btnOpenSecondImage.Size = new System.Drawing.Size(94, 23);
+			this.btnOpenSecondImage.TabIndex = 3;
+			this.btnOpenSecondImage.Text = "Open 2nd Image";
+			this.btnOpenSecondImage.UseVisualStyleBackColor = true;
+			this.btnOpenSecondImage.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// btnCompareImage
+			// 
+			this.btnCompareImage.Location = new System.Drawing.Point(12, 307);
+			this.btnCompareImage.Name = "btnCompareImage";
+			this.btnCompareImage.Size = new System.Drawing.Size(93, 23);
+			this.btnCompareImage.TabIndex = 4;
+			this.btnCompareImage.Text = "Compare Image";
+			this.btnCompareImage.UseVisualStyleBackColor = true;
+			this.btnCompareImage.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// rtbMetrics
+			// 
+			this.rtbMetrics.Location = new System.Drawing.Point(161, 307);
+			this.rtbMetrics.Name = "rtbMetrics";
+			this.rtbMetrics.Size = new System.Drawing.Size(162, 101);
+			this.rtbMetrics.TabIndex = 10;
+			this.rtbMetrics.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.rtbMetrics);
+			this.Controls.Add(this.btnCompareImage);
+			this.Controls.Add(this.btnOpenSecondImage);
+			this.Controls.Add(this.btnOpenFirstImage);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Metrics";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Button btnOpenFirstImage;
+		private System.Windows.Forms.Button btnOpenSecondImage;
+		private System.Windows.Forms.Button btnCompareImage;
+		private System.Windows.Forms.RichTextBox rtbMetrics;
+	}
+}
+
---
+++ CalculateMSEandPSNR/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateMSEandPSNR
+{
+	public partial class Form1 : Form
+	{
+		Bitmap originalPict;
+		Bitmap changedPict;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public void GetPSNR(Bitmap originalImage, Bitmap markedImage)
+		{
+			int width = originalImage.Width, height = originalImage.Height;
+			double psnr = 0;
+			double mse = 0;
+			double mseR = 0;
+			double mseG = 0;
+			double mseB = 0;
+			double rmse = 0;
+
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					mseR = mseR + ((markedImage.GetPixel(x, y).R - originalImage.GetPixel(x, y).R) * (markedImage.GetPixel(x, y).R - originalImage.GetPixel(x, y).R));
+					mseG = mseG + ((markedImage.GetPixel(x, y).G - originalImage.GetPixel(x, y).G) * (markedImage.GetPixel(x, y).G - originalImage.GetPixel(x, y).G));
+					mseB = mseB + ((markedImage.GetPixel(x, y).B - originalImage.GetPixel(x, y).B) * (markedImage.GetPixel(x, y).B - originalImage.GetPixel(x, y).B));
+				}
+			}
+
+			mse = (1.0 / (3 * width * height)) * (mseR + mseG + mseB);
+			lblMSE.Text = mse.ToString();
+			
+			rmse = Math.Sqrt(mse);
+			lblRMSE.Text = rmse.ToString();
+			
+			psnr = 10 * Math.Log10((Math.Pow(255, 2)) / mse);
+			lblPSNR.Text = psnr.ToString();
+
+			rtbMetrics.Text = "MSE = " + mse.ToString() +"\n" + "RMSE = " + rmse.ToString() + "\n" + "PSNR = " + psnr.ToString(); 
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			using (var openFileDialog = new OpenFileDialog())
+			{
+				openFileDialog.Filter = "Images(*.jpg; *.jpeg;)|*.jpg; *jpeg; |All files(*.*)|*.*";
+
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					try
+					{
+						originalPict = new Bitmap(openFileDialog.FileName);
+						pictureBox1.Image = originalPict;
+					}
+					catch
+					{
+						MessageBox.Show("Невозможно открыть файл", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			using (var openFileDialog = new OpenFileDialog())
+			{
+				openFileDialog.Filter = "Images(*.jpg; *.jpeg;)|*.jpg; *jpeg; |All files(*.*)|*.*";
+
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					try
+					{
+						changedPict = new Bitmap(openFileDialog.FileName);
+						pictureBox2.Image = changedPict;
+					}
+					catch
+					{
+						MessageBox.Show("Невозможно открыть файл", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+				}
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			GetPSNR(originalPict, changedPict);
+		}
+	}
+}
